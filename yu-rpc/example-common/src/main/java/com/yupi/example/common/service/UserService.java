c38612877a943ef6260c9f39b59d02d3eb717172
wangxun-19 <1850847412@qq.com>
2025-03-04 23:40:39 +0800

    第二次提交

---
+++ yu-rpc/example-common/src/main/java/com/yupi/example/common/service/UserService.java
@@ -0,0 +1,18 @@
+package com.yupi.example.common.service;
+
+import com.yupi.example.common.model.User;
+
+/**
+ * 用户服务
+ */
+public interface UserService {
+
+    /**
+     * 获取用户
+     *
+     * @param user
+     * @return
+     */
+    User getUser(User user);
+
+}
